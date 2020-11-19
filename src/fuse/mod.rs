38c7771ff0a8d7c62f5a7dec91cda1b78c9f46ee
Roberto <rob.ariosa@hotmail.com>
2020-11-19 17:01:07 +0100

    Fork if not foreground

--- src/fuse/mod.rs
+++ src/fuse/mod.rs
@@ -2,11 +2,20 @@ use anyhow::anyhow;
 use anyhow::Result;
 use filesystem::Crypt4ghFS;
 use std::ffi::OsStr;
+use fork::{daemon, Fork};
 
 pub mod filesystem;
 pub mod libc_wrappers;
 pub mod libc_extras;
 
 pub fn run(fs: Crypt4ghFS, threads: usize, mountpoint: String, foreground: bool, options: Vec<&OsStr>) -> Result<()> {
-	fuse_mt::mount(fuse_mt::FuseMT::new(fs, threads), &mountpoint, &options).map_err(|e| anyhow!("{:?}", e))
+	if foreground {
+		fuse_mt::mount(fuse_mt::FuseMT::new(fs, threads), &mountpoint, &options).map_err(|e| anyhow!("{:?}", e)).unwrap();
+	}
+	else {
+		if let Ok(Fork::Child) = daemon(true, true) {
+			fuse_mt::mount(fuse_mt::FuseMT::new(fs, threads), &mountpoint, &options).map_err(|e| anyhow!("{:?}", e)).unwrap();
+		}
+	}
+	Ok(())
 }
